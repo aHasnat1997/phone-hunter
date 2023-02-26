0f6606f61dae8c3d3e23185cd2a2d2435ad4ad77
aHasnat1997 <hasnat56703@gmail.com>
2023-02-26 15:19:50 +0600

    modal add

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ const showPhone = (phones, limitData) => {
   )
 
   phones.forEach(phone => {
-    const { image, phone_name } = phone;
+    const { image, phone_name, slug } = phone;
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="card bg-base-200 shadow-xl overflow-hidden">
@@ -37,7 +37,7 @@ const showPhone = (phones, limitData) => {
       <div class="card-body">
         <h2 class="card-title">${phone_name}</h2>
         <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+          <label for="my-modal" onclick="detailsPhone('${slug}')" class="btn btn-primary">show details</label>
         </div>
       </div>
     </div>
@@ -45,6 +45,16 @@ const showPhone = (phones, limitData) => {
     cardContainer.appendChild(div);
   });
   spinner(false);
+};
+
+const spinner = isSpin => {
+  const spinner = document.getElementById('spinner');
+  if (isSpin) {
+    spinner.classList.remove('hidden');
+  }
+  else {
+    spinner.classList.add('hidden');
+  }
 };
 
 const searchData = (limitData) => {
@@ -58,16 +68,50 @@ const searchPhone = () => {
   searchData(16);
 };
 
-document.getElementById('load-more-btn').addEventListener('click', function(){
+document.getElementById('search-fild').addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    searchData(16);
+  }
+});
+
+document.getElementById('load-more-btn').addEventListener('click', function () {
   searchData();
 });
 
-const spinner = isSpin => {
-  const spinner = document.getElementById('spinner');
-  if (isSpin) {
-    spinner.classList.remove('hidden');
-  }
-  else {
-    spinner.classList.add('hidden');
-  }
+const detailsPhone = async id => {
+  const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  modalFuntion(data.data);
+}
+
+const modalFuntion = (details) => {
+  console.log('click');
+
+  const {image, name} = details;
+  document.getElementById('modal-img').setAttribute('src', `${image}`);
+  document.getElementById('name').innerText = name;
+
+  // const modalContener = document.getElementById('modal-contener');
+  // modalContener.innerHTML = '';
+
+  // const div = document.createElement('div');
+  // div.innerHTML = `
+  // <input type="checkbox" id="my-modal" class="modal-toggle" />
+  //   <label for="my-modal" class="modal cursor-pointer">
+  //     <label class="modal-box relative max-w-5xl" for="">
+
+  //       <div class="card lg:card-side">
+  //         <img id="modal-img" src="${image}" width="50%" />
+  //         <div class="card-body">
+  //           <h2 class="card-title">New album is released!</h2>
+  //           <p>Click the button to listen on Spotiwhy app.</p>
+  //         </div>
+  //       </div>
+
+  //     </label>
+  //   </label>
+  // `;
+  // modalContener.appendChild(div);
+
 };
